dd196123c839d121bc04bd94e5de5ecaa80d44d6
thatkaye <thatkaye00@gmail.com>
2023-03-13 22:29:27 +0800

    simplified to traditional

--- thesis/views.py
+++ thesis/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.shortcuts import render
 from PIL import Image
-from googletrans import Translator
 from .braille_converter import translateToBraille
 
 import base64
@@ -11,13 +10,22 @@ import numpy as np
 import pytesseract
 import cv2
 
+# kaye's
 pytesseract.pytesseract.tesseract_cmd = (
-    r"A:\Tesseract-OCR\tesseract.exe"  # Path to tesseract.exe
+    r"X:\Tesseract-OCR\tesseract.exe"  # Path to tesseract.exe
 )
 
-myconfig = r"-l eng+chi_sim"
+myconfig = r"-l chi_tra+english"
 
 # Create your views here.
+def resize(im, newWidth):
+    width, height = im.size
+    ratio = height / width
+    newHeight = int(ratio * newWidth)
+    resizedImage = im.resize((newWidth, newHeight))
+
+    return resizedImage
+
 def index(request):
     if request.method == "POST":
         try:
@@ -27,12 +35,11 @@ def index(request):
             image_base64 = base64.b64encode(image.read()).decode("utf-8")
         except:
             messages.add_message(
-                request, messages.ERROR, "no image is uploaded"
+                request, messages.ERROR, "no image is  uploaded"
             )
 
             return render(request, "home.html")
-
-        # lang = request.POST["language"]    
+  
         img = np.array(Image.open(image))
         text = pytesseract.image_to_string(img, config = myconfig)
 
