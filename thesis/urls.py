9b4412d3ace8a3a5f7fe9a630963cddc00fce8e9
thatkaye <52037251+thatkaye@users.noreply.github.com>
2023-06-02 20:24:28 +0800

    Delete thesis directory

--- thesis/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index, convert_to_braille_image, translate
-
-# urlconf
-urlpatterns = [
-    path('', index, name="index"),
-    path('braille/', convert_to_braille_image, name='braille'),
-    path('translate/', translate, name='translate')
-]
